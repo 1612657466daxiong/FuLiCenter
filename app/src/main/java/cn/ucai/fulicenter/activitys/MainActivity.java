2e90a5b207443fadb07deab002ea19831579a99f
1612657466daxiong <1612657466@qq.ocm>
2016-10-17 11:06:25 +0800

    add Butternife

--- app/src/main/java/cn/ucai/fulicenter/activitys/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activitys/MainActivity.java
@@ -1,15 +1,59 @@
 package cn.ucai.fulicenter.activitys;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.rb_boutique)
+    RadioButton rbBoutique;
+    @Bind(R.id.rb_new_good)
+    RadioButton rbNewGood;
+    @Bind(R.id.rb_category)
+    RadioButton rbCategory;
+    @Bind(R.id.rb_personal_center)
+    RadioButton rbPersonalCenter;
+    @Bind(R.id.rb_cart)
+    RadioButton rbCart;
+    @Bind(R.id.rg_menu)
+    RadioGroup rgMenu;
+    @Bind(R.id.tv_cart_hint)
+    TextView tvCartHint;
+    @Bind(R.id.rlayout)
+    RelativeLayout rlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.rb_boutique, R.id.rb_new_good, R.id.rb_category, R.id.rb_personal_center, R.id.rb_cart, R.id.tv_cart_hint})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rb_boutique:
+                break;
+            case R.id.rb_new_good:
+                break;
+            case R.id.rb_category:
+                break;
+            case R.id.rb_personal_center:
+                break;
+            case R.id.rb_cart:
+                break;
+            case R.id.tv_cart_hint:
+                break;
+        }
     }
 }
