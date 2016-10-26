7432c07941f763c02f6649bf25c246dc7cc6ee18
1612657466daxiong <1612657466@qq.ocm>
2016-10-26 19:41:46 +0800

    share page implementation

--- app/src/main/java/cn/ucai/fulicenter/activitys/GoodsDetActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activitys/GoodsDetActivity.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
@@ -110,7 +112,7 @@ public class GoodsDetActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.id_ivback,R.id.id_ivcollect})
+    @OnClick({R.id.id_ivback,R.id.id_ivcollect,R.id.id_ivshare})
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.id_ivback:
@@ -119,7 +121,41 @@ public class GoodsDetActivity extends AppCompatActivity {
             case R.id.id_ivcollect:
                 iscollect();
                 break;
+            case R.id.id_ivshare:
+                showShare();
+                break;
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     private void addgoodsincollect() {
